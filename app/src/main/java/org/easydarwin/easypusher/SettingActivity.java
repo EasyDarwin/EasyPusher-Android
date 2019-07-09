bb811ea4b47695f3d1f5d0b54b3e420f2fc7831b
liyy-23 <1290374862@qq.com>
2019-07-09 14:07:18 +0800

    保存推流地址

--- app/src/main/java/org/easydarwin/easypusher/SettingActivity.java
+++ app/src/main/java/org/easydarwin/easypusher/SettingActivity.java
@@ -149,6 +149,16 @@ public class SettingActivity extends AppCompatActivity implements Toolbar.OnMenu
         });
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        String text = url.getText().toString().trim();
+        if (text.toLowerCase().startsWith("rtsp://")) {
+            Config.setServerURL(SettingActivity.this, text);
+        }
+    }
+
     /*
      * 二维码扫码
      * */
